85c12cfd7459ce6ce8ecb44ab9fc09bf18eddd7c
danielguarchaj <gtdanniel@gmail.com>
2015-12-09 17:28:42 -0600

    Validar campos con regex: nombres, telefonos, correos y direccion.

--- Adress Book using Regex (Classwork)/lib/adressBookScript.js
+++ Adress Book using Regex (Classwork)/lib/adressBookScript.js
@@ -1,5 +1,8 @@
+var validateAll = false;
+
 var disableButtonSubmit = function(){
   $("button#buttonSubmit").attr("disabled", "disabled");
+  validateAll = false;
   console.log("button submit disabled");
 }
 
@@ -11,4 +14,40 @@ var disableButtonDelete = function(){
 var disableButtonEdit = function(){
   $("button#buttonEdit").attr("disabled", "disabled");
   console.log("button edit disabled");
+}
+
+var enableButtonSubmit = function(){
+  $("button#buttonSubmit").removeAttr("disabled");
+  validateAll = true;
+  console.log("button submit enabled");
+}
+
+var enableButtonDelete = function(){
+  $("button#buttonDelete").removeAttr("disabled");
+  console.log("button delete enabled");
+}
+
+var enableButtonEdit = function(){
+  $("button#buttonEdit").removeAttr("disabled");
+  console.log("button edit enabled");
+}
+
+var validateNames = function (name){
+  var validatedName = /^[a-zA-Z\s]*$/;
+  return validatedName.test(name);
+}
+
+var validateEmail = function (email){
+  var validateEmail = /^[\w+@\w+\.\w+]*$/;
+  return validateEmail.test(email);
+}
+
+var validatePhone = function (number){
+  var validateDigits = /^[\d]*$/;
+  return validateDigits.test(number);
+}
+
+  var validateStreetAddress = function (address){
+  var validateAddress = /^[a-zA-Z\s.*0-9-*]*$/;
+  return validateAddress.test(address);
 }
--- Adress Book using Regex (Classwork)/scripts/adressBookInterface.js
+++ Adress Book using Regex (Classwork)/scripts/adressBookInterface.js
@@ -3,8 +3,91 @@ $(document).ready(function(){
   disableButtonDelete();
   disableButtonEdit();
 
-  $("button#buttonSubmit").on("click",function(){
-
+  $("form").on("keyup", "input#inputFirstName", function(){
+    if(validateNames($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
   })
+
+  $("form").on("keyup", "input#inputMiddleInitial", function(){
+    if(validateNames($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputLastName", function(){
+    if(validateNames($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputPersonalEmail", function(){
+    if(validateEmail($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputBusinessEmail", function(){
+    if(validateEmail($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputMobile", function(){
+    if(validatePhone($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputHome", function(){
+    if(validatePhone($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputWork", function(){
+    if(validatePhone($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputBusinessEmail", function(){
+    if(validateEmail($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("form").on("keyup", "input#inputStreetAdress", function(){
+    if(validateStreetAddress($(this).val())){
+      enableButtonSubmit();
+    } else {
+      disableButtonSubmit();
+    }
+  })
+
+  $("button#buttonSubmit").on("click",function(event){
+    event.preventDefault();
+  })
+
+
+
 
 })
