05bbf75cca52be7632eb9b922b39711ea4269b1f
danielguarchaj <gtdanniel@gmail.com>
2015-12-09 10:57:37 -0600

    Finalizar pronosticos de 1, 5 y 16 dias.

--- ApiWeather/scripts/apiweatherScript.js
+++ ApiWeather/scripts/apiweatherScript.js
@@ -1,3 +1,26 @@
+
+
+
 var toCelcius = function(kelvin){
-  return celcius = kelvin - 273.15;
+  var celcius = kelvin - 273.15;
+  return celcius.toFixed(2);
+}
+
+var currentDayWeather = function(city){
+  var response = "";
+  debugger
+  $.get(apiUrl16Days + city + apiKey, function(data){
+    console.log(data);
+    var tempMin = toCelcius(data.main.temp_min);
+    var tempMax = toCelcius(data.main.temp_max);
+    return response = "City: "+city+" - Temp Min: "+tempMin+" to "+tempMax+" - Wind Speed: "+data.wind.speed+" m/s.";
+  })
+
+  return response;
+}
+
+var dayWeather = function(weather){
+
+  
+
 }
--- ApiWeather/scripts/myscript.js
+++ ApiWeather/scripts/myscript.js
@@ -1,14 +1,45 @@
 $(document).ready(function(){
-    var apiKey = "025abfe602c79e92f4aab34daca7f7a1";
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+    var apiKey = "&APPID=025abfe602c79e92f4aab34daca7f7a1";
+
+    var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
+
     $("button#forecast").on("click", function(){
+
       var city = $("input#city").val();
-      $.get(apiUrl + city + "&APPID=" + apiKey, function(data){
-        console.log(data)
-        var tempMin = toCelcius(data.main.temp_min);
-        var tempMax = toCelcius(data.main.temp_max);
-        $("ol.myCity").append("<li>"+city + " - Temp Min: 0C"+tempMin+" to "+tempMax+" Max 0C - Wind Speed "+data.wind.speed+" m/s</li>");
-      })
+
+      if($('input:radio[name=days]:checked').val()==="1"){
+        $("span#forecastDays").text("One Day");
+        $.get(apiUrl + city + "&cnt=1&APPID=" + apiKey, function(data){
+          console.log(data)
+          var tempMin = toCelcius(data.list[0].temp.min);
+          var tempMax = toCelcius(data.list[0].temp.max);
+          $("ol.myCity").append("<li>"+city + " - Temp Min: "+tempMin+" 0C to Max "+tempMax+" 0C - Wind Speed "+data.list[0].speed+" m/s</li>");
+        })
+      } else if($('input:radio[name=days]:checked').val()==="5"){
+        $("span#forecastDays").text("Five Days / daily forecast data");
+        $.get(apiUrl + city + "&cnt=5&APPID=" + apiKey, function(data){
+          for(var i=0; i<5; i++){
+          console.log(data)
+          var tempMin = toCelcius(data.list[i].temp.min);
+          var tempMax = toCelcius(data.list[i].temp.max);
+          $("ol.myCity").append("<li>"+city + " - Temp Min: "+tempMin+" 0C to Max "+tempMax+" 0C - Wind Speed "+data.list[i].speed+" m/s</li>");
+        }
+        })
+      }else if($('input:radio[name=days]:checked').val()==="16"){
+        $("span#forecastDays").text("Sixteen Days / daily forecast data");
+        $.get(apiUrl + city + "&cnt=16&APPID=" + apiKey, function(data){
+          for(var i=0; i<16; i++){
+          console.log(data)
+          var tempMin = toCelcius(data.list[i].temp.min);
+          var tempMax = toCelcius(data.list[i].temp.max);
+          $("ol.myCity").append("<li>"+city + " - Temp Min: "+tempMin+" 0C to Max "+tempMax+" 0C - Wind Speed "+data.list[i].speed+" m/s</li>");
+        }
+        })
+      }else{
+        $("span#forecastDays").text("Must choose one option!");
+      }
+
     })
 
     $("button#reset").on("click",function(){
