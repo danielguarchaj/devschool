bd9a6df038291a75d540a12cb4fb70034623af30
danielguarchaj <gtdanniel@gmail.com>
2015-12-17 12:08:41 -0600

    Demonstrate the order that posts appear using a sleep.

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -8,11 +8,19 @@ Meteor.methods({
       url: String
     });
 
-    var postWithSameLink = Post.findOne({url: postAttributes.url});
+    if(Meteor.isServer){
+      postAttributes.title += "(server)";
+      // wait for 5 seconds
+      Meteor._sleepForMs(5000);
+    }else{
+      postAttributes.title += "(client)";
+    }
+
+    var postWithSameLink = Posts.findOne({url: postAttributes.url});
     if(postWithSameLink){
       return{
         postExists: true,
-        _id: postWithSameLink._id;
+        _id: postWithSameLink._id
       }
     }
 
@@ -20,13 +28,13 @@ Meteor.methods({
     var post = _.extend(postAttributes, {
       userId: user._id,
       author: user.username,
-      submitted: new Date();
+      submitted: new Date()
     });
 
     var postId = Posts.insert(post);
 
     return {
-      _id: postId;
+      _id: postId
     };
   }
 });
