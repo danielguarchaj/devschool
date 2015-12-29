99424d22a870bbada608ebc590f96da43b2cba08
danielguarchaj <gtdanniel@gmail.com>
2015-12-29 12:34:32 -0600

    Check and delete todos.

--- todos/todos.js
+++ todos/todos.js
@@ -4,11 +4,33 @@ if (Meteor.isClient) {
   //Template helpers:
   Template.main.helpers({
     todos: function(){
-      return Todos.find();
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.main.events({
+    'submit .new-todo': function(event){
+      event.preventDefault();//prevent submit default
+      var text = event.target.text.value;
+
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      //clear form
+      event.target.text.value = '';
+    },
+    'click .toggle-checked': function(){
+      Todos.update(this._id, {$set:{checked: ! this.checked}});//check the todos
+    },
+    'click .delete-todo': function(){
+      if(confirm('are you sure?')){
+      Todos.remove(this._id);
+      }
     }
   });
 }
-
 if (Meteor.isServer) {
 
 }
