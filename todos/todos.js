133aef8e4913b60c04c45aabe6ff8a2c4c8fdc05
danielguarchaj <gtdanniel@gmail.com>
2015-12-29 13:09:05 -0600

    Restrict check and delete.

--- todos/todos.js
+++ todos/todos.js
@@ -13,24 +13,49 @@ if (Meteor.isClient) {
       event.preventDefault();//prevent submit default
       var text = event.target.text.value;
 
-      Todos.insert({
-        text: text,
-        createdAt: new Date()
-      });
+      Meteor.call('addTodo', text);
 
       //clear form
       event.target.text.value = '';
     },
     'click .toggle-checked': function(){
-      Todos.update(this._id, {$set:{checked: ! this.checked}});//check the todos
+      Meteor.call('setChecked', this._id, !this.checked);
     },
     'click .delete-todo': function(){
-      if(confirm('are you sure?')){
-      Todos.remove(this._id);
+      if(confirm('Are you sure?')){
+      Meteor.call('deleteTodo', this._id);
       }
     }
   });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
+
 }
+
+//meteor methods
+
+Meteor.methods({
+  addTodo: function(text){
+    if(!Meteor.userId()){
+      throw new Meteor.Error('Not Authorized');
+    }
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId){
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked){
+    Todos.update(todoId, {$set:{checked: setChecked}});//check the todos
+  }
+});
+
 if (Meteor.isServer) {
 
 }
