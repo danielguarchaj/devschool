5f11db240bca0841ee4ab94ebda5b38b17a3b7cc
danielguarchaj <gtdanniel@gmail.com>
2015-12-29 16:13:44 -0600

    Routing and templates.

---
+++ microposts/lib/router.js
@@ -0,0 +1,15 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+
+Router.map(function(){
+  //posts route
+  this.route('posts', {
+      path: '/',
+      template: 'posts'
+  });
+
+  //about Router
+  this.route('about');
+
+});
